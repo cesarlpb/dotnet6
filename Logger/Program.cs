a7d3c4caf875b30ad58f4bda93dee6b49917d31f
Cesar <mail>
2021-12-14 09:39:15 +0100

    loggers

---
+++ Logger/Program.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using System;
+using System.IO;
+
+namespace Logger
+{
+    public static class FileLogger
+    {
+        private const string FilePath = "log.txt";
+        public static void Log(string mensaje)
+        {
+            using var fileStream = new FileStream(FilePath, FileMode.Append);
+            using var writer = new StreamWriter(fileStream);
+                writer.WriteLine(mensaje);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileLogger.Log("Mensaje");
+            var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+            logger.Verbose("ejemplo");
+            logger.Error("error aqui");
+            logger.Fatal("fatal");
+        }
+    }
+}
