b2a428e9d2a4e4b4f9a3e7365635073f58dfa055
César <cesar@imaginagroup.com>
2021-12-19 09:05:29 -0500

    update

---
+++ 7-8/Soluciones/2-Cobertura/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program {
+public static int exitCode = 0;
+public static void Main()
+{
+    int coverage = CalcCoverage("cobertura.xml");
+    if (coverage > 90){
+    exitCode = Convert.ToInt32(ExitCode.Success);
+    }
+    else{
+    exitCode = Convert.ToInt32(ExitCode.Failed);
+    }
+    Environment.ExitCode = exitCode;
+}
+public static int CalcCoverage(string cobertura){
+    // Parse cobertura xml para retornar el valor 
+    // Round to int
+    return 99;
+}
+public enum ExitCode : int {
+  Success = 0,  // coverage = 100%
+  Failed = 1    // coverage below certain %
+}
+}
